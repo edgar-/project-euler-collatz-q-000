06986a3727aa22a8b6f874fd5b9ae08387c55d11
M. Edgar Joel <mkakajoel@gmail.com>
2016-04-14 11:14:04 +0000

    Solution to P Euler's collatz challenge

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,36 @@
 def even_next(n)
+  n/2
 end
 
 def odd_next(n)
+  3*n + 1
 end
 
 def next_value(n)
+  return even_next(n) if n % 2 == 0
+  return odd_next(n)
 end
 
 def collatz(n)
+  clz = [n]
+  while n > 1
+    n = next_value(n)
+    clz << n
+  end
+  clz
 end
 
 def longest_collatz
+  n = 999999
+  longest_collatz  = [1]
+
+  n.downto(2) do |i|
+    if !longest_collatz.include?(i)
+      current_collatz = collatz(i)
+      if current_collatz.length > longest_collatz.length
+        longest_collatz = current_collatz
+      end
+    end
+  end
+  longest_collatz[0]
 end
